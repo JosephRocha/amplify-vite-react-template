5bb6ea1fdfd867e8d82fcff7958a14342bead75e
Joseph Rocha <JosephRocha.cs@gmail.com>
2024-05-28 05:49:40 -0500

    Changed Logo and color scheme, made loading messages, added span for daily gain and gain perc

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import { useState } from "react";
 
 function App() {
 
-  const [StockPrice, setStockPrice] = useState({"description": "", "icon": "", "name": "", "last_price": ""});
+  const [StockPrice, setStockPrice] = useState({ "description": "", "icon": "", "name": "", "last_price": "", "price_diff": 0, "price_diff_perc": "", "last_close": 0});
   const [Ticker, setTicker] = useState("");
+  const [isTickerSearched, setIsTickerSearched] = useState(false)
+  const [Status, setStatus] = useState("Search a symbol above");
 
   function GetTicker() {
-    setStockPrice({"description": "Loading...", "icon": "Loading", "name": "Loading...", "last_price": "Loading..."});
+    setIsTickerSearched(false);
+    setStatus("Loading...")
     fetch("https://794kdtq270.execute-api.us-east-1.amazonaws.com/prod/ticker/" + Ticker).then(response => response.json())
-      .then(data => setStockPrice(data))
+      .then(function (data) {
+        setStockPrice(data);
+        setIsTickerSearched(true)
+      })
   }
 
   function handleTest(e: any) {
     if (e.key == "Enter") {
+      setIsTickerSearched(false);
       e.preventDefault();
       GetTicker();
       console.log(e);
@@ -25,28 +32,32 @@ function App() {
         <div className="row">
           <div className="col">
 
-            <div className="input-group">
-            <input className="form-control p-3 mb-2 bg-dark text-white" id="Ticker" placeholder="Stock" onKeyDown={handleTest} onInput={e => setTicker((e.target as HTMLInputElement).value)} />
-                <div className="input-group-append">
-                  <button id="searchButton" className="btn bg-dark text-white form-control p-3 mb-2" type="button">Search</button>
+            <div className="input-group p-3">
+              <input className="form-control p-3 mb-2 text-white" id="Ticker" placeholder="Stock" onKeyDown={handleTest} onInput={e => setTicker((e.target as HTMLInputElement).value)} />
+              <div className="input-group-append">
+                <button id="searchButton" className="btn bg-dark text-white form-control p-3" type="button">Search</button>
+              </div>
+            </div>
+
+            {isTickerSearched == true ? (
+              <div id="MainWindow" className="row p-3 mb-2 text-white">
+                <div className="text-center p-3 mb-2 text-white">
+                  <h1 ><img className="img" id="Bullicon" src={StockPrice.icon} /> {StockPrice.name}</h1>
+
+                  <h2 className="text-center">${StockPrice.last_price} 
+                    {StockPrice.price_diff > 0 ? ( <span id="badgex" className="badge text-bg-success">{StockPrice.price_diff} ({StockPrice.price_diff_perc}%)</span>) : (<span id="badgex" className="badge text-bg-danger">[{StockPrice.price_diff}] ({StockPrice.price_diff_perc}%)</span>)}</h2>
+                  <h5 className="text-center">Last Close Price: ${StockPrice.last_close}</h5>
                 </div>
-            </div>
-
-          <div className="row p-3 mb-2 bg-dark text-white">
-            <div className="text-center p-3 mb-2 bg-dark text-white">
-              <h1 ><img className="img" id="Bullicon" src={StockPrice.icon}  /> {StockPrice.name}</h1>
-              <h2 className="text-center">${StockPrice.last_price}</h2>
-            </div>
-
-
+                <div className="row p-3 mb-2 text-white">
+                  <h3>Description</h3>
+                  <p>{StockPrice.description}</p>
+                </div>
+              </div>
+            ) : (
+              <div className="text-center p-3 mb-2 text-white">{Status}</div>
+            )}
           </div>
-          <div className="row p-3 mb-2 bg-dark text-white ">
-            <h3>Description</h3> <p>{StockPrice.description}</p>
-
-          </div>
-
         </div>
-      </div>
       </div>
 
     </main>
