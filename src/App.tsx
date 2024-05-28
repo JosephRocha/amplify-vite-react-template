cd31159c672b423404cd81142316196710a6b094
Joseph Rocha <JosephRocha.cs@gmail.com>
2024-05-28 03:21:53 -0500

    Using Polygon for testing, cleaned up some other stuff added description

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import {  useState } from "react";
+import { useState } from "react";
 
 function App() {
 
-  const [StockPrice, setStockPrice] = useState("Enter Symbol");
+  const [StockPrice, setStockPrice] = useState({"description": "", "icon": "", "name": "", "last_price": ""});
   const [Ticker, setTicker] = useState("");
 
   function GetTicker() {
-      setStockPrice("Loading...");
-      fetch("https://794kdtq270.execute-api.us-east-1.amazonaws.com/prod/ticker/" + Ticker).then(response => response.json())
+    setStockPrice({"description": "Loading...", "icon": "Loading", "name": "Loading...", "last_price": "Loading..."});
+    fetch("https://794kdtq270.execute-api.us-east-1.amazonaws.com/prod/ticker/" + Ticker).then(response => response.json())
       .then(data => setStockPrice(data))
   }
 
@@ -21,16 +21,32 @@ function App() {
 
   return (
     <main>
-      <form className="form-inline">
-        <div className="form-group mx-sm-3 mb-2">
-          <input className="form-control" id="Ticker" placeholder="Stock" onKeyDown={handleTest} onInput={e=> setTicker((e.target as HTMLInputElement).value)} />
+      <div className="container">
+        <div className="row">
+          <div className="col">
+
+            <div className="input-group">
+            <input className="form-control p-3 mb-2 bg-dark text-white" id="Ticker" placeholder="Stock" onKeyDown={handleTest} onInput={e => setTicker((e.target as HTMLInputElement).value)} />
+                <div className="input-group-append">
+                  <button id="searchButton" className="btn bg-dark text-white form-control p-3 mb-2" type="button">Search</button>
+                </div>
+            </div>
+
+          <div className="row p-3 mb-2 bg-dark text-white">
+            <div className="text-center p-3 mb-2 bg-dark text-white">
+              <h1 ><img className="img" id="Bullicon" src={StockPrice.icon}  /> {StockPrice.name}</h1>
+              <h2 className="text-center">${StockPrice.last_price}</h2>
+            </div>
+
+
+          </div>
+          <div className="row p-3 mb-2 bg-dark text-white ">
+            <h3>Description</h3> <p>{StockPrice.description}</p>
+
+          </div>
+
         </div>
-      </form>
-
-
-      <div className="card text-center">
-        <h1 >{Ticker}</h1>
-         <h2>Last Closing Price: {StockPrice}</h2>
+      </div>
       </div>
 
     </main>
